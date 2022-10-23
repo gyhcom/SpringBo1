c560538204d6e13c2230ce811eafa7988db80a94
gyhcom <gyeonho@gamil.com>
2022-10-23 22:21:52 +0900

    QueryDsl 사용세팅

--- b01/src/test/java/com/springboot/b01/repository/BoardRepositoryTests.java
+++ b01/src/test/java/com/springboot/b01/repository/BoardRepositoryTests.java
@@ -5,7 +5,12 @@ import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
@@ -56,5 +61,22 @@ public class BoardRepositoryTests {
         Long bno = 1L;
 
         boardRepository.deleteById(bno);
+    }
+
+    @Test
+    public void testPagin(){
+        //1 page order by bno desc
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("bno").descending());
+
+        Page<Board> result = boardRepository.findAll(pageable);
+
+        log.info("total count: " + result.getTotalElements());
+        log.info("total pages: " + result.getTotalPages());
+        log.info("total number: " + result.getNumber());
+        log.info("total size: " + result.getSize());
+
+        List<Board> todoList = result.getContent();
+        todoList.forEach(board -> log.info(board));
+
     }
 }
