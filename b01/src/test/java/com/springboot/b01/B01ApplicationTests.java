85c2cb71a1a7223954e346293eb03b0620067e58
gyhcom <gyeonho@gamil.com>
2022-10-23 21:59:13 +0900

    구멍가게코딩단 웹개발워크북

---
+++ b01/src/test/java/com/springboot/b01/B01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.b01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ b01/src/test/java/com/springboot/b01/repository/BoardRepositoryTests.java
@@ -0,0 +1,60 @@
+package com.springboot.b01.repository;
+
+import com.springboot.b01.domain.Board;
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+import java.util.stream.IntStream;
+
+@SpringBootTest
+@Log4j2
+public class BoardRepositoryTests {
+
+    @Autowired
+    private BoardRepository boardRepository;
+
+    @Test
+    public void testInsert(){
+        IntStream.rangeClosed(1,100).forEach(i ->{
+            Board board = Board.builder()
+                    .title("title..." +i)
+                    .content("context..." + i)
+                    .writer("user" + (i % 10))
+                    .build();
+
+            Board result = boardRepository.save(board);
+            log.info("BNO: " + result.getBno());
+        });
+    }
+
+    @Test
+    public void testSelect() {
+        Long bno = 100L;
+
+        Optional<Board> result = boardRepository.findById(bno);
+
+        Board board = result.orElseThrow();
+        log.info(board);
+    }
+
+    @Test
+    public void testUpdate(){
+        Long bno = 100L;
+
+        Optional<Board> result = boardRepository.findById(bno);
+        Board board = result.orElseThrow();
+        board.change("update...title 100", "update content 100");
+        boardRepository.save(board);
+
+    }
+
+    @Test
+    public void testDelete() {
+        Long bno = 1L;
+
+        boardRepository.deleteById(bno);
+    }
+}
