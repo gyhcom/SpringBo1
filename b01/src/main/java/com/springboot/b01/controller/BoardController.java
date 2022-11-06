78eab92f0d9e96e8310ef388255399ecf8f12324
gyhcom <gyeonho@gamil.com>
2022-11-06 13:28:51 +0900

    검색 및 pageing 추가

--- b01/src/main/java/com/springboot/b01/controller/BoardController.java
+++ b01/src/main/java/com/springboot/b01/controller/BoardController.java
@@ -8,8 +8,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/board")
@@ -21,8 +26,29 @@ public class BoardController {
     @GetMapping("/list")
     public void list(PageRequestDTO pageRequestDTO, Model model) {
         PageResponseDTO<BoardDTO> responseDTO = boardService.list(pageRequestDTO);
-        log.info("======"+responseDTO);
+        log.info("======" + responseDTO);
 
         model.addAttribute("responseDTO", responseDTO);
+    }
+
+    @GetMapping("/regitster")
+    public void registerGET() {
+
+    }
+
+    @PostMapping("/register")
+    public String registerPost(@Valid BoardDTO boardDTO, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+        log.info("board POST register....");
+        if (bindingResult.hasErrors()) {
+            log.info("has erros.....");
+            redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors());
+            return "redirect:/board/regitser";
+        }
+
+        log.info(boardDTO);
+        Long bno = boardService.register(boardDTO);
+        redirectAttributes.addFlashAttribute("result", bno);
+
+        return "redirect:/board/list";
     }
 }
